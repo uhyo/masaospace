56c9a1994cec9b2ad02fc4d9ba258f93d7611405
uhyo <uhyo@uhyohyo.net>
2015-06-16 10:28:44 +0900

    use debug build

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //Starter
-var index = require('./js/index');
+var index;
+try{
+    index = require('./ts/index');
+}catch(e){
+    index = require('./js/index');
+}
 var system = new index.System();
 system.init(function (err) {
     if (err) {
