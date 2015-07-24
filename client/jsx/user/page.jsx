1f20d173c9777a6feb34d25eedd6c3cf273e49b5
uhyo <uhyo@uhyohyo.net>
2015-07-25 04:00:55 +0900

    show your profile

--- client/jsx/user/page.jsx
+++ client/jsx/user/page.jsx
@@ -5,8 +5,7 @@ var QueryList=require('../game/parts/query-list.jsx');
 
 module.exports = React.createClass({
     displayName:"UserPage",
-    render:function(){
-        //TODO
+    render(){
         var user=this.props.data;
         var query={
             owner: this.props.userid
@@ -14,8 +13,28 @@ module.exports = React.createClass({
         return (
             <section className="user-page">
                 <h1>{user.name}</h1>
-                <QueryList query={query} />
+                {this.profile(user)}
+                <section className="user-page-list">
+                    <h1>{user.name}の正男</h1>
+                    <QueryList query={query} />
+                </section>
             </section>
         );
+    },
+    profile(user){
+        if(user.profile){
+            return (
+                <div className="user-page-profile">
+                    <p>{user.profile}</p>
+                </div>
+            );
+        }else{
+            //プロフィールがない
+            return (
+                <div className="user-page-profile user-page-profile-empty">
+                    <p>プロフィールが登録されていません。</p>
+                </div>
+            );
+        }
     }
 });
