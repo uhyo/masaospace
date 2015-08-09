0c8677078eacf1ba6a8e009a9994feaa5d6a1604
uhyo <uhyo@uhyohyo.net>
2015-08-09 16:52:43 +0900

    gulp watch env

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var path=require('path');
 var gulp=require('gulp');
+var gulputil=require('gulp-util');
+var duration=require('gulp-duration');
 var browserify=require('browserify');
 var source=require('vinyl-source-stream');
 var reactify=require('reactify');
 var babelify=require('babelify');
+var uglifyify=require('uglifyify');
+var watchify=require('watchify');
 var globule=require('globule');
 var typescript=require('gulp-typescript');
 var del=require('del');
@@ -24,15 +28,11 @@ gulp.task('tsc',function(){
 });
 
 gulp.task('jsx',function(){
-    return browserify({
-        entries:[path.join(__dirname,"client/entrypoint.jsx")],
-        transform:[babelify,reactify],
-        extensions:['.js','.jsx'],
-        basedir:__dirname
-    })
-    .bundle()
-    .pipe(source("components.js"))
-    .pipe(gulp.dest("dist/"));
+    return jsxCompiler(false);
+});
+
+gulp.task('watch-jsx',function(){
+    return jsxCompiler(true);
 });
 
 gulp.task('mc_canvas',function(){
@@ -66,5 +66,51 @@ gulp.task('clean',function(cb){
     ],cb);
 });
 
+gulp.task('watch',['watch-jsx','css','tsc'],function(){
+    //w
+    gulp.watch("client/css/*.scss",['css']);
+    gulp.watch("src/**/*.ts",['tsc']);
+});
+
 gulp.task('client',['jsx','css']);
 gulp.task('default',['tsc','jsx','css','mc_canvas','static']);
+
+//jsx compiling
+function jsxCompiler(watch){
+    //init browserify bundler
+    var opts={
+        entries:[path.join(__dirname,"client/entrypoint.jsx")],
+        extensions:[".js",".jsx"],
+        basedir:__dirname
+    };
+    if(watch){
+        opts.cache={};
+        opts.packageCache={};
+        opts.fullPaths=true;
+    }
+    var b=browserify(opts);
+    if(watch){
+        b=watchify(b);
+    }
+    //chain
+    b.transform(babelify)
+    .transform(reactify)
+    .transform(uglifyify,{global:true});
+
+    b.on('update',bundle);
+    bundle();
+    return b;
+
+    function bundle(){
+        gulputil.log('recompiling jsx');
+
+        b
+        .bundle()
+        .on('error',function(err){
+            console.error(err);
+        })
+        .pipe(duration("compiled jsx"))
+        .pipe(source("components.js"))
+        .pipe(gulp.dest("dist/"));
+    }
+}
