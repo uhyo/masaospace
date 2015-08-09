704a9123de74e074e6a69db390fbe9c67e40140f
uhyo <uhyo@uhyohyo.net>
2015-08-09 14:21:49 +0900

    user page icon & url

--- client/jsx/user/page.jsx
+++ client/jsx/user/page.jsx
@@ -1,7 +1,8 @@
 var React=require('react');
 var Reflux=require('reflux');
 
-var QueryList=require('../game/parts/query-list.jsx');
+var QueryList=require('../game/parts/query-list.jsx'),
+    UserIcon=require('../commons/user-icon.jsx');
 
 module.exports = React.createClass({
     displayName:"UserPage",
@@ -17,7 +18,12 @@ module.exports = React.createClass({
         return (
             <section className="user-page">
                 <h1>{user.name}</h1>
-                {this.profile(user)}
+                <div className="user-page-info">
+                    <div className="user-page-icon">
+                        <UserIcon icon={user.icon} size={128}/>
+                    </div>
+                    {this.profile(user)}
+                </div>
                 <section className="user-page-list">
                     <h1>{user.name}の正男</h1>
                     <QueryList query={query} />
@@ -27,9 +33,16 @@ module.exports = React.createClass({
     },
     profile(user){
         if(user.profile){
+            var url=null;
+            if(user.url){
+                url=<p>
+                    <a href={user.url} className="external" target="_blank">{user.url}</a>
+                </p>;
+            }
             return (
                 <div className="user-page-profile">
                     <p>{user.profile}</p>
+                    {url}
                 </div>
             );
         }else{
