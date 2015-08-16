4cca656c3ffe71a661a162250b6c360773a31a80
uhyo <uhyo@uhyohyo.net>
2015-08-16 20:49:44 +0900

    just show tags

--- client/jsx/game/play.jsx
+++ client/jsx/game/play.jsx
@@ -30,6 +30,20 @@ module.exports = React.createClass({
                 </a>
             </p>;
         }
+        var tags=null;
+        if(metadata.tags && metadata.tags.length>0){
+            //タグがあった
+            tags=<div className="game-play-info-tags">
+                <div><span className="icon icon-tag"/></div>
+                <ul>{
+                    metadata.tags.map((tag,i)=>{
+                        return <li key={i}>
+                            {tag}
+                        </li>;
+                    })
+                }</ul>
+            </div>;
+        }
         return (
             <section>
                 <h1>{metadata.title}</h1>
@@ -44,7 +58,10 @@ module.exports = React.createClass({
                         <UserTile {...this.props.owner} label="投稿者" fullWidth/>
                     </div>
                     <div className="game-play-info-description">
-                        <p>{metadata.description}</p>
+                        <div className="game-play-info-message">
+                            <p>{metadata.description}</p>
+                        </div>
+                        {tags}
                     </div>
                 </div>
                 <GameTools config={this.props.config} metadata={metadata}/>
