390c821867f59217e85b650a81d66f48b6e016ac
uhyo <uhyo@uhyohyo.net>
2016-03-24 00:56:32 +0900

    safe-game-view confirmation

--- client/jsx/game/safe-game-view.jsx
+++ client/jsx/game/safe-game-view.jsx
@@ -13,12 +13,47 @@ module.exports = React.createClass({
         //TODO: audio_enabledに対応してない
         audio_enabled: React.PropTypes.bool,
     },
+    componentWillReceiveProps(nextProps){
+        this.setState({
+            confirm: false
+        });
+    },
+    getInitialState(){
+        return {
+            //実行を承諾
+            confirm: false
+        };
+    },
     render(){
-        const {game, config, audio_enabled} = this.props;
+        var {game, config, audio_enabled} = this.props;
+        var content;
+        if(this.state.confirm){
+            //了承済
+            content = <iframe className="game-safe-view" sandbox="allow-scripts" src={`//${config.service.sandboxDomain}/sandbox/${game.id}`} width="512" height="320" />;
+        }else{
+            //警告を表示
+            content = <div className="game-safe-view-confirm" onClick={this.handleClick}>
+                <p>
+                    <span className="icon icon-warning"/>
+                    JavaScript拡張がある正男です。
+                </p>
+                <p>JavaScriptが実行されると、ブラウザや端末に負荷をかけたり、脆弱性を持つウェブサービスを攻撃されたりする恐れがあります。</p>
+                <p>この正男の作者を信頼できない場合、正男を表示しないでください。</p>
+                <div className="game-safe-view-confirm-button">
+                    正男を表示
+                </div>
+            </div>;
+        }
         return (
-            <div className="game-view">
-                <iframe sandbox="allow-scripts" src={`//${config.service.sandboxDomain}/sandbox/${game.id}`} width="512" height="320" />
-            </div>
+            <div className="game-view">{
+                content
+            }</div>
         );
-    }
+    },
+    handleClick(e){
+        e.preventDefault();
+        this.setState({
+            confirm: true
+        });
+    },
 });
